a06b6f1d9898f6570359cb2f056898a3ab61585b
Krish Chothani <143370415+KrishChothani@users.noreply.github.com>
2024-08-29 14:17:29 +0530

    Header , Left Header and videodetailPage

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,17 +1,19 @@
-import React,{ StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
 import './index.css'
-import EmptyVideoPage from './Components/HomePage.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from './Components/HomePage.jsx'
 import ReactDom from 'react-dom/client';
+import HomePage from "./Components/HomePage.jsx";
+import VideoDetailPage from './Components/VideoDetailPage.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/home",
     element: <HomePage />
   },
+  {
+    path: "/videodetail",
+    element: <VideoDetailPage />
+  }
 ]);
 
 ReactDom.createRoot(document.getElementById("root")).render(
