727cbb2d817b109d6affdc36cc0e19b198b4ff20
Krish Chothani <143370415+KrishChothani@users.noreply.github.com>
2024-09-23 15:21:36 +0530

    kkk

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Header from "../Components/Header/Header";
 import Left_Header from "../Components/Left_Header/Left_Header";
 import axios from 'axios';
-import AllVideoMiddleSection from "../Components/allVideoMiddleSection";
+import AllVideoMiddleSection from ".../Components/allVideoMiddleSection";
 
 function HomePage() {
   
