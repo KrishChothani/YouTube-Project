ca6d37e915549c3f9d109a7645c0a717699054b0
Krish Chothani <143370415+KrishChothani@users.noreply.github.com>
2024-10-03 01:27:00 +0530

    update

--- Frontend/src/Pages/ChannelPages/ChannelVideoListPage.jsx
+++ Frontend/src/Pages/ChannelPages/ChannelVideoListPage.jsx
@@ -1,9 +1,4 @@
 import React from 'react'
-import Header from '../../Components/Header/Header';
-import Left_Header from '../../Components/Left_Header/Left_Header';
-import CoverImage from '../../Components/Channel/CoverImage';
-import AvatarAndChannelDetail from '../../Components/Channel/AvatarAndChannelDetail';
-import ChannelPageListSelection from '../../Components/Channel/ChannelPageListSelection';
 import AllVideoMiddleSection from '../../Components/allVideoMiddleSection';
 
 function ChannelVideoListPage() {
