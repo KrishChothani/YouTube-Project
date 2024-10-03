1431b2b62f267d8cd133fa210a10072bdf6fc28d
Krish Chothani <143370415+KrishChothani@users.noreply.github.com>
2024-10-03 18:08:20 +0530

    update

--- Frontend/src/Components/Registration.jsx
+++ Frontend/src/Components/Registration.jsx
@@ -32,7 +32,7 @@
 
      axios({
        method: "POST",
-       url: "http://localhost:8000/api/v1/users/register",
+       url: "/api/v1/users/register",
        data: formData,
        headers: {
          "Content-Type": "multipart/form-data", // Remove this line if it causes issues
