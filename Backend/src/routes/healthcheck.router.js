e8012a5e4771fa34f1a74804eca18cb96ddf70a9
Krish Chothani <143370415+KrishChothani@users.noreply.github.com>
2024-11-22 18:39:24 +0530

    login check

--- Backend/src/routes/healthcheck.router.js
+++ Backend/src/routes/healthcheck.router.js
@@ -1,19 +1,9 @@
-{
-  "version": 2,
-  "builds": [
-    {
-      "src": "src/index.js",
-      "use": "@vercel/node"
-    }
-  ],
-  "routes": [
-    {
-      "src": "/api/(.*)",
-      "dest": "src/index.js"  // Ensure that API routes go to the server file
-    },
-    {
-      "src": "/(.*)",
-      "dest": "/public/$1"  // Route static assets to the 'public' folder
-    }
-  ]
-}
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { healthcheck } from "../controllers/healthcheck.controller.js";
+
+const router = new Router();
+// router.use(verifyJWT)
+
+router.route('/').get(healthcheck);
+export default router;
