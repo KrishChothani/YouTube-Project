2a57f6f17a1d0138ef0a71b6d72ecde369332719
Krish Chothani <143370415+KrishChothani@users.noreply.github.com>
2025-05-17 15:36:43 +0530

    change clodkinary

--- Backend/src/utils/cloudinary.js
+++ Backend/src/utils/cloudinary.js
@@ -1,28 +1,30 @@
 import {v2 as cloudinary} from 'cloudinary'; 
 import fs from 'fs';
 
-cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret:process.env.CLOUDINARY_API_SECRET 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-
-const uploadOnCloudinary = async (localFilePath) =>{
-    try {
-        if(!localFilePath) return null;
-        // upload the file on the cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath ,  {
-            resource_type: 'auto',
-        });
-        // remove locally save temporary file as the upload failed
-        
-        return response;
-    } catch (er) {
-        fs.unlinkSync(localFilePath); // remove locally save temporary file as the upload failed
-        return null;
-    }
-}
+const uploadOnCloudinary = async (localFilePath) => {
+  console.log(localFilePath);
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto", // Ensures PDF is treated as a raw file
+      folder: "youtube_video", // Optional: Organize files
+      use_filename: true,
+      unique_filename: false,
+      access_mode: "public", // Ensure public access
+    });
+    return response;
+  } catch (er) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+  
 
 
 export {uploadOnCloudinary};
